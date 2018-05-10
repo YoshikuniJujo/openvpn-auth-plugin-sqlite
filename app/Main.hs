a269f1a9417780e0d018f0a133bf671f0b236ed9
Yoshikuni Jujo <PAF011432[at]nifty.ne.jp>
2018-05-10 14:39:27 +0900

    Only one user and password

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,62 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
-import Lib
+import Control.Monad
+
+import Data.Maybe
+import Data.List
+import Data.Bits
+import Data.Word
+import Data.Char
+import Data.ByteString (ByteString)
+
+import System.IO
+import System.Environment
+import System.Exit
+-- import System.Posix.User
+import Crypto.Hash
+
+import qualified Data.ByteArray as BA
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BSC
 
 main :: IO ()
-main = someFunc
+main = do
+	fp : _as <- getArgs
+	h <- openFile fp ReadMode
+	un <- BSC.hGetLine h
+	p <- BSC.hGetLine h
+	when (un /= "yosh") exitFailure
+--	u <- (<> "\n") . BSC.pack <$> getEffectiveUserName
+	h0 <- hello
+	let	h1 = BA.convert $ sha512 p
+	when (h1 /= h0) $ do
+		writeFile "/home/tatsuya/openvpn_auth.log" $
+			show h0 <> "\n" <> show h1 <> "\n"
+		exitFailure
+
+	BS.writeFile "/home/tatsuya/openvpn_auth.log" $
+		un <> "\n" <> p <> "\n"
+
+sha512 :: ByteString -> Digest SHA512
+sha512 = hash
+
+hexDigits :: [Char]
+hexDigits = "0123456789abcdef"
+
+hexToNum1 :: Char -> Word8
+hexToNum1 = fromIntegral . fromJust . (`elemIndex` hexDigits)
+
+readHex :: Char -> Char -> Word8
+readHex a b = (hexToNum1 a `shift` 4) .|. hexToNum1 b
+
+readHash :: String -> ByteString
+readHash = BS.pack . rh
+	where
+	rh (a : b : cs) = readHex a b : rh cs
+	rh "" = []
+	rh _ = error "bad"
+
+hello :: IO ByteString
+hello = readHash . takeWhile (not . isSpace)
+	<$> readFile "hello2.txt"
