fbeacd0e3863bba4ac6807095d2d0043f3d11938
Yoshikuni Jujo <PAF011432[at]nifty.ne.jp>
2018-05-10 17:02:48 +0900

    execution to check auth

---
+++ app/check-auth.hs
@@ -0,0 +1,22 @@
+import Control.Exception
+import System.IO
+import System.Environment
+import System.Process
+import System.IO.Temp
+
+main :: IO ()
+main = do
+	cmd : u : _ <- getArgs
+	withSystemTempFile "auth" $ \fp h -> do
+		putStrLn fp
+		hPutStrLn h u
+		withEcho False getLine >>= hPutStrLn h
+		hClose h
+		(_, _, _, ph) <- createProcess $ proc "stack" ["exec", cmd, fp]
+		waitForProcess ph >>= print
+		putStrLn u
+
+withEcho :: Bool -> IO a -> IO a
+withEcho echo action = do
+	old <- hGetEcho stdin
+	bracket_ (hSetEcho stdin echo) (hSetEcho stdin old) action
