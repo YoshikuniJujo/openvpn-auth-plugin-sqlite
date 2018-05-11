fcad146e3185e4aa367557abe9d31d96df402821
Yoshikuni Jujo <PAF011432[at]nifty.ne.jp>
2018-05-11 14:49:32 +0900

    tribial

--- app/check-auth.hs
+++ app/check-auth.hs
@@ -8,13 +8,11 @@ main :: IO ()
 main = do
 	cmd : u : _ <- getArgs
 	withSystemTempFile "auth" $ \fp h -> do
-		putStrLn fp
 		hPutStrLn h u
 		withEcho False getLine >>= hPutStrLn h
 		hClose h
 		(_, _, _, ph) <- createProcess $ proc "stack" ["exec", cmd, fp]
 		waitForProcess ph >>= print
-		putStrLn u
 
 withEcho :: Bool -> IO a -> IO a
 withEcho echo action = do
